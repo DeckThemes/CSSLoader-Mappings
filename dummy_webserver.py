cde8f1bebc936fca712a9411ec515e9fcd341b93
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2024-06-04 08:29:34 +0200

    Add dummy webserver

---
+++ dummy_webserver.py
@@ -0,0 +1,19 @@
+import os, time
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+if not os.path.exists("requests"):
+    os.makedirs("requests")
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/body", methods=["POST"])
+@cross_origin()
+def save_request_body():
+    with open(f"requests/{time.time()}.txt", "wb") as f:
+        f.write(request.data)
+    return "OK"
+
+app.run(host='0.0.0.0', port=5000)
